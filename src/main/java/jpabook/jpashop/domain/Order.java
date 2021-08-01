aa9f97f764322c6db00a14c2d665da9721bdc508
kiaofk <kiaofk@naver.com>
2021-08-01 20:22:36 +0900

    JPA 도메인 설계

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,57 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="orders")
+@Getter @Setter
+public class Order {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToMany(mappedBy = "order",cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    private LocalDateTime orderDate; // 주문시간
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status; //주문 상태 [ORDER, CANCEL]
+
+    //==연관관계 메서드==// 양방향 일 경우 쓰면 양쪽에 모두 값 저장가능하게 하는 메소드
+    public void setMemeber(Member memeber){
+        this.member=member;
+        member.getOrders().add(this);
+
+    }
+
+    public void addOrderItem(OrderItem orderItem){
+        this.orderItems=orderItems;
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+                delivery.setOrder(this);
+    }
+
+
+
+
+}
---
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.domain.item;
+
+import jpabook.jpashop.domain.Category;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE) //상속 관계일 때 무조건 어노테이션 해줘야함(전략에 따라)
+@DiscriminatorColumn(name="dtype")
+@Getter
+@Setter
+public abstract class Item {
+
+    @Id @GeneratedValue
+    @Column(name="item_id")
+    private Long id;
+
+    private String name;
+
+    private int price;
+
+    private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
+
+
+}
